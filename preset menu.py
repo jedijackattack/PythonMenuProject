18188922bc27233d985e01d45090efa834cfbeab
jedijackatack <jedijackattack@gmail.com>
2017-09-29 10:58:31 +0100

    Update preset menu.py

--- preset menu.py
+++ preset menu.py
@@ -1,5 +1,5 @@
 PresetMenu = input("Would you like the preset menu?")
 if PresetMenu.upper()=="YES":
     SalmonorHen = input("Would you like salmon or hen")
-    Dishes.price = Dishes.price + 45
-    print (Dishes.price)
+    price = 45
+    print ("£"+price)
