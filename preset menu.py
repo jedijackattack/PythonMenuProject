c19d76c47374223622ffa9b55d1b1d1ff15af9c5
DanielWindram <32386847+DanielWindram@users.noreply.github.com>
2017-09-29 10:54:32 +0100

    Add files via upload

---
+++ preset menu.py
@@ -0,0 +1,5 @@
+PresetMenu = input("Would you like the preset menu?")
+if PresetMenu.upper()=="YES":
+    SalmonorHen = input("Would you like salmon or hen")
+    Dishes.price = Dishes.price + 45
+    print (Dishes.price)
